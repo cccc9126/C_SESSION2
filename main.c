5b7d2cbf76986cdfe8add18e92a92c89f7172734
Cachiu <huyhuy9126@gmail.com>
2024-11-20 15:23:18 +0700

    Add files via upload

--- main.c
+++ main.c
@@ -1,11 +1,16 @@
+
 #include <stdio.h>
 
 int main(int argc, const char * argv[]) {
-    const float PI = 3.14;
-    float banKinh = 5;
-    float chuVi, dienTich;
-    chuVi = 2*PI*banKinh;
-    dienTich = PI*banKinh*banKinh;
-    printf("chu vi hinh tron la %f\ndien tich hinh tron la %f\n",chuVi, dienTich);
+    int a = 1;
+    // kiểu dữ liệu số nguyên để lưu trữ số nguyên dương hoặc nguyên âm
+    float b = 1.5;
+    //kiểu dữ liệu số thực để lưu trữ số có dấu thập phân
+    double c = 4.5;
+    //kiểu dữ liệu số thực để lưu trữ số có dấu thập phân và có số byte nhiều hơn float và lưu được 15 số sau dấu phẩy
+    char d = 'w';
+    //kiểu kí tự để lưu một kí tự
+    printf ("a la %d, b la %f, c la %lf, d la %c\n",a, b, c, d);
     return 0;
 }
+
